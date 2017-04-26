d1aa3f14fea8a69ca080f356c5add5a7d013b913
Andrew Broman <abroman@medicine.wisc.edu>
2017-04-26 08:19:24 -0500

    Initial commit

---
+++ src/PicklistUI.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PicklistUI extends Component {
+  render() {
+    return (
+      <div className="picklist-ui">
+        <div className="picklist-ui-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default PicklistUI;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PicklistUI from './PicklistUI';
+import './index.css';
+
+ReactDOM.render(
+  <PicklistUI />,
+  document.getElementById('root')
+);
