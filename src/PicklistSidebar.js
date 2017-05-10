0ca70766b010154bfefba3682ddd03b85f8c1730
Andrew Broman <abroman@medicine.wisc.edu>
2017-05-10 12:16:43 -0500

    Fix typo

--- src/PicklistSidebar.js
+++ src/PicklistSidebar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class PicklistSidebar extends Component {
   render() {
     return (
-      <div class="card">
+      <div className="card">
         <div className="picklist-sidebar card-block">
           <div className="card-title">Picklist</div>
           <div className="card-body"></div>
