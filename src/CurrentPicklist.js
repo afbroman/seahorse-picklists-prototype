e05a0618a81d44c0aff3309645c6f674c156763f
Andrew Broman <abroman@medicine.wisc.edu>
2017-05-11 13:36:18 -0500

    Improve layout & slide effect of sidebar

--- src/CurrentPicklist.js
+++ src/CurrentPicklist.js
@@ -3,14 +3,13 @@ import $ from 'jquery';
 import 'jquery-ui-bundle';
 
 class CurrentPicklist extends Component {
-  displaySidebar() {
-    $('#sidebar').switchClass('col-0', 'col-6');
-    $('#results').switchClass('col', 'col-6');
+  toggleSidebar() {
+    $('#sidebar').toggle('slide', {direction: 'right'});
   }
 
   render() {
     return (
-      <button id="current-picklist" onClick={this.displaySidebar}>Current Picklist: Jones 2017-05</button>
+      <button id="current-picklist" onClick={this.toggleSidebar}>Current Picklist: Jones 2017-05</button>
     );
   }
 }
