bcfbf01a094e1a9f5af76edead3254d8b3195448
Andrew Broman <abroman@medicine.wisc.edu>
2017-05-16 09:54:29 -0500

    Add basic drag and drop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import 'jquery-ui-bundle';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PicklistSidebar from './PicklistSidebar';
 import CurrentPicklist from './CurrentPicklist';
+import SampleTypeModal from './SampleTypeModal';
 import './index.css';
+
+$('.entry').draggable({revert: true});
+$('#sidebar').droppable({
+  drop: (event, ui) => {
+    console.log('dropped');
+  }
+});
 
 ReactDOM.render(
   <PicklistSidebar />,
@@ -13,4 +23,9 @@ ReactDOM.render(
 ReactDOM.render(
   <CurrentPicklist />,
   document.getElementById('current-picklist-container')
+);
+
+ReactDOM.render(
+  <SampleTypeModal />,
+  document.getElementById('modal')
 );
