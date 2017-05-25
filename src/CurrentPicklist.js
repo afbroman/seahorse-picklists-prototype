78f696a862be2ba985e7658572f7571e70c38088
Andrew Broman <abroman@medicine.wisc.edu>
2017-05-25 08:56:46 -0500

    Adjust layout and sidebar slide-in

--- src/CurrentPicklist.js
+++ src/CurrentPicklist.js
@@ -6,7 +6,9 @@ import { Button } from 'react-bootstrap';
 class CurrentPicklist extends Component {
   toggleSidebar() {
     $('#filter-form').toggle('slide', {direction: 'up', duration: 200});
-    $('#sidebar').toggle('slide', {direction: 'right', duration: 200});
+    $('#main-content').toggleClass('col-12 col-8');
+    $('#sidebar').toggleClass('col-0 col-4');
+    $('#sidebar').show();
   }
 
   render() {
