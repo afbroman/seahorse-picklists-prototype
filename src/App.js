986583fdfc620f7b18694d0fe66e59543b787986
Andrew Broman <abroman@medicine.wisc.edu>
2017-06-21 11:51:33 -0500

    Remove unused logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import PicklistSidebar from './PicklistSidebar';
 import SampleTypeModal from './SampleTypeModal';
 import FilterForm from './FilterForm';
